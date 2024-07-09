375fe5394bab2c2f8dada5918b4c8ff7ac2e9cc5
jeonjuseong11 <jeonjuseong11@gmail.com>
2024-07-09 22:35:20 +0900

    Feat : HomePageLayout
    - 홈페이지 레이아웃 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image";
-import Link from "next/link";
 import styles from "./page.module.css";
-import jLogo from "../../public/zlogo.png";
+import Image from "next/image";
+import zLogo from "../../public/zlogo.png";
+import Link from "next/link";
 
-export default function Home() {
+export default function Main() {
   return (
-    <div>
+    <div className={styles.container}>
       <div className={styles.left}>
-        <Image src={jLogo} alt="logo" />
+        <Image src={zLogo} alt="logo" />
       </div>
-      <div>
+      <div className={styles.right}>
         <h1>지금 일어나고 있는 일</h1>
-        <h2>지금 가입하세요</h2>
+        <h2>지금 가입하세요.</h2>
         <Link href="/i/flow/signup" className={styles.signup}>
           계정 만들기
         </Link>
