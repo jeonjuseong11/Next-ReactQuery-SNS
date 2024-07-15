9f3d2f2dc146188f54ddf7bb91a26ec29ce62be9
jeonjuseong11 <jeonjuseong11@gmail.com>
2024-07-15 23:11:32 +0900

    Feat : LeftSideMenu
    - useSelectedLayoutSegment로 ActiveLink 구현(사이드 메뉴)

---
+++ src/app/(afterLogin)/_component/LogoutButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+import style from "./logOutButton.module.css";
+
+export default function LogoutButton() {
+  const me = {
+    id: "JEO1129",
+    nickname: "주성",
+    image: "/5Udwvqim.jpg",
+  };
+
+  const onLogout = () => {};
+
+  return (
+    <div className={style.logOutButton} onClick={onLogout}>
+      <div className={style.logOutUserImage}>
+        <img className={me.image} alt={me.id} />
+      </div>
+      <div className={style.logOutUserName}>
+        <div>{me.nickname}</div>
+        <div>@{me.id}</div>
+      </div>
+    </div>
+  );
+}
