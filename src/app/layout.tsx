730ce7d3a4ac4b81a9b0518cbbc871b53e450431
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2024-08-01 08:27:29 +0900

    Feat : MSWComponent
    - next용 .env, .env.local 생성
    - msw component 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { MSWComponent } from "./_component/MSWComponent";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,10 @@ type Props = { children: React.ReactNode };
 export default function RootLayout({ children }: Readonly<Props>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <MSWComponent />
+        {children}
+      </body>
     </html>
   );
 }
